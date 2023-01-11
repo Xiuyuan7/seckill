58f4b6af50efae8f8e3c5052781e77156a2a2238
Xiuyuan Wang <tom.xy.wang@outlook.com>
2023-01-10 20:01:15 -0600

    Added createOrder method and related attributes to SeckillActivityService class

--- src/main/java/com/jiuzhang/seckill/services/SeckillActivityService.java
+++ src/main/java/com/jiuzhang/seckill/services/SeckillActivityService.java
@@ -1,13 +1,34 @@
 package com.jiuzhang.seckill.services;
 
+import com.alibaba.fastjson.JSON;
+import com.jiuzhang.seckill.db.dao.SeckillActivityDao;
+import com.jiuzhang.seckill.db.po.Order;
+import com.jiuzhang.seckill.db.po.SeckillActivity;
+import com.jiuzhang.seckill.mq.RocketMQService;
 import com.jiuzhang.seckill.util.RedisService;
+import com.jiuzhang.seckill.util.SnowFlake;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SeckillActivityService {
+
     @Autowired
     private RedisService redisService;
+
+    @Autowired
+    private SeckillActivityDao seckillActivityDao;
+
+    @Autowired
+    private RocketMQService rocketMQService;
+
+    /**
+     * datacenterId; 数据中心
+     * machineId; 机器标识
+     * 在分布式环境中可以从机器配置上读取
+     * 单机开发环境中先写死
+     */
+    private SnowFlake snowFlake = new SnowFlake(1, 1);
 
     /**
      * 判断商品是否还有库存
@@ -18,4 +39,30 @@ public class SeckillActivityService {
         String key = "stock:" + activityId;
         return redisService.stockDeductValidator(key);
     }
+
+    /**
+     * 创建订单
+     *
+     * @param seckillActivityId
+     * @param userId
+     * @return
+     * @throws Exception
+     */
+    public Order createOrder(long seckillActivityId, long userId) throws Exception {
+        // 创建订单
+        SeckillActivity seckillActivity = seckillActivityDao.querySeckillActivityById(seckillActivityId);
+        Order order = new Order();
+
+        // 采用雪花算法生成订单ID
+        order.setOrderNo(String.valueOf(snowFlake.nextId()));
+        order.setSeckillActivityId(seckillActivity.getId());
+        order.setUserId(userId);
+        order.setOrderAmount(seckillActivity.getSeckillPrice().longValue());
+
+        // 发送创建订单消息
+        rocketMQService.sendMessage("seckill_order", JSON.toJSONString(order));
+
+        return order;
+    }
+
 }
